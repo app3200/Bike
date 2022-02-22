ad59aeb67d4fcf4ff6db7d605dbf81f0f18152e1
app3200 <appattanaik3200@gmail.com>
2022-02-22 21:19:24 +0530

    1st commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div id="navbar">
+      <div id="logo">
+        <img src="https://container-app.ackoassets.com/am/myaccount_frontend/4972dde433c8123ca0821f57ef2e9a5c.svg" />
+      </div>
+    </div>
+  );
+};
