285f3d659adfff9dd59f369dba88cb0bf9cece54
Durgesh kumar rai <durgeshrai633@gmail.com>
2022-02-23 19:15:50 +0530

    added profile dropdowns

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
      {/* <SelectBike/> */}
      <Navbar></Navbar>
-     <Hero></Hero>
+     {/* <Hero></Hero> */}
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState,useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Dropdown } from "./Dropdown";
 import "./navbar.css";
 import { ResourceDropdown } from "./ResourceDropdown";
@@ -185,6 +185,71 @@ export const Navbar = () => {
           {showProductsDropdown ? <Triangle /> : ""}
         </div>
         {showProductsDropdown ? <Dropdown /> : ""}
+        <div className='profile-dropdown'>
+          <div className='profile-wrapper'>
+            <div className='profile-container'>
+              <div className='profile-dropdown-container'>
+                <div className='profile-name-icon-div'>
+                  <div className='profile-name-icon-div-div'>
+                    <div className='profile-icon-div'>
+                      <div className='profile-icon'>
+                        <svg
+                          fill='none'
+                          height='46'
+                          viewBox='0 0 46 46'
+                          width='46'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            d='m22.9246 44.0833c11.6058 0 21.0142-9.4393 21.0142-21.0833s-9.4084-21.08334-21.0142-21.08334-21.0142 9.43934-21.0142 21.08334 9.4084 21.0833 21.0142 21.0833z'
+                            fill='#f1f3f8'
+                          ></path>
+                          <g fill='#9364ed'>
+                            <path d='m28.686 16.8985c0 3.2113-2.5606 6.262-5.7613 6.262-3.2008 0-5.7614-3.0507-5.7614-6.262s2.5606-5.7803 5.7614-5.7803c3.2007 0 5.7613 2.569 5.7613 5.7803z'></path>
+                            <path d='m32.3517 32.4556c.16.8028-.3201 1.6056-1.2803 1.9267-1.6004.4817-4.8012 1.124-8.1619 1.124-3.3608 0-6.5615-.8028-8.1619-1.2845-.8002-.1606-1.2803-.9634-1.2803-1.9268.6401-4.4958 3.5718-6.9861 9.4422-6.9861 5.7238 0 8.802 2.6509 9.4422 7.1467z'></path>
+                          </g>
+                        </svg>
+                      </div>
+                    <div className='dgObov'>
+                      <p className='ecadKN'>Durgesh rai</p>
+                      <p className='jXCDif'>8299027233</p>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='lines-div'></div>
+                <div className='profile-extras'>
+                  <div style={{ textDecoration: "none", cursor: "pointer" }}>
+                    <div className='my-policies-div'>
+                      <div className='mypolicies'>
+                        <img
+                          height='20px'
+                          width='20px'
+                          src='https://myaccount.ackoassets.com/static/images/header-icons/my-policy-v2.svg'
+                          className='fpCRcM'
+                        ></img>
+                        <p className='eHEzhk'>My policies</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ textDecoration: "none", cursor: "pointer" }}>
+                    <div className='my-policies-div'>
+                      <div className='mypolicies'>
+                        <img
+                          height='20px'
+                          width='20px'
+                          src='https://myaccount.ackoassets.com/static/images/header-icons/my-profile-v2.svg'
+                          className='fpCRcM'
+                        ></img>
+                        <p className='eHEzhk'>My profile</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
