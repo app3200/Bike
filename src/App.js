0c5a1a5ec76d6cfc59d4c7d86c168ae5c6a22c05
Durgesh kumar rai <durgeshrai633@gmail.com>
2022-02-23 12:02:09 +0530

    creating hero section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { SelectBike } from './Components/Policy Flow/SelectBike';
 import { Navbar } from './Components/Navbar/Navbar';
+import { Hero } from './Components/HeroSection/Hero';
 
 function App() {
   return (
     <div className="App">
      {/* <SelectBike/> */}
      <Navbar></Navbar>
+     <Hero></Hero>
     </div>
   );
 }
---
+++ src/Components/HeroSection/Hero.jsx
@@ -0,0 +1,8 @@
+import"./hero.css"
+export const Hero = () => {
+  return <div className="hero-div">
+      <div className="left-image">
+          
+      </div>
+  </div>;
+};
