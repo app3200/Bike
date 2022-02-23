0ed8d6fd2e160d6db509e10b7982c77eae78bbcc
Durgesh kumar rai <durgeshrai633@gmail.com>
2022-02-23 22:29:28 +0530

    still working

--- src/Components/HeroSection/Hero.jsx
+++ src/Components/HeroSection/Hero.jsx
@@ -1,8 +1,29 @@
-import"./hero.css"
+import "./hero.css";
 export const Hero = () => {
-  return <div className="hero-div">
-      <div className="left-image">
-          
+  return (
+    <div className='hero-div'>
+      <div className='left-image'>
+        <img
+          src='https://acko-home-prod.ackoassets.com/next_assets/6cfee294eb0ba0a61f1100811fda667701b09450/_next/static/images/homepage-banner-illustration-1-5d2a4d352884a025c5e728efb0a4c6ce.svg'
+          alt=''
+          className='cOTRjA'
+        ></img>
       </div>
-  </div>;
+      <div className='FPpoI'>
+        <div className='dhkjzO'>
+          <div className='eLKcAo'>
+            <h1 class='jFZtMc'>Insurance made easy</h1>
+            <p class='dBdUlO'>Zero commission. Zero paperwork</p>
+          </div>
+        </div>
+      </div>
+      <div className='left-image'>
+        <img
+          src='https://acko-home-prod.ackoassets.com/next_assets/6cfee294eb0ba0a61f1100811fda667701b09450/_next/static/images/homepage-banner-illustration-2-99577afed0f02e7ee1411f1e8aede6c9.svg'
+          alt=''
+          className='cOTRjA'
+        ></img>
+      </div>
+    </div>
+  );
 };
