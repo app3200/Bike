3db45cee084c79f3e1fc7e3c5ccd79175f8b97fb
Ankit Mungole <93372275+ankitmung@users.noreply.github.com>
2022-02-23 19:28:42 +0530

    footer added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import { Ackolove } from "./AckoLove";
 import { CompanyLogs } from "./CompanyLogs";
 import { Reviews } from "./Reviews";
+import {Footer} from "./Footer"
 
 export function Home(){
     return  <div>
         <Reviews></Reviews>
         <CompanyLogs></CompanyLogs>
         <Ackolove></Ackolove>
+        <Footer></Footer>
     </div>
 }
