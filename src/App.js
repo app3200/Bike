933bce0a4f09766babc2754e6963b7b5caa1222f
Durgesh kumar rai <durgeshrai633@gmail.com>
2022-02-24 16:26:01 +0530

    pushed to the master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { SelectBike } from './Components/Policy Flow/SelectBike';
-import { Navbar } from './Components/Navbar/Navbar';
-import { Hero } from './Components/HeroSection/Hero';
+import logo from "./logo.svg";
+import "./App.css";
+import { SelectBike } from "./Components/Policy Flow/SelectBike";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Hero } from "./Components/HeroSection/Hero";
 
 function App() {
   return (
-    <div className="App">
-     {/* <SelectBike/> */}
-     <Navbar></Navbar>
-     <Hero></Hero>
+    <div className='App'>
+      {/* <SelectBike/> */}
+      {/* <Navbar></Navbar>
+     <Hero></Hero> */}
     </div>
   );
 }
