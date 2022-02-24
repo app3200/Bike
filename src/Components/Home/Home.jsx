44584ebca8492b04d398c611aa8469f567e73221
Ankit Mungole <93372275+ankitmung@users.noreply.github.com>
2022-02-24 20:25:45 +0530

    landing page update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,17 @@
 import { Hero } from "../HeroSection/Hero";
 import { Navbar } from "../Navbar/Navbar";
+import { Ackolove } from "./LandingSection/AckoLove";
+import { CompanyLogs } from "./LandingSection/CompanyLogos";
+import { Footer } from "./LandingSection/Footer";
+import { Reviews } from "./LandingSection/Reviews";
 
 export function Home(){
     return <>
         <Navbar></Navbar>
         <Hero></Hero>
+        <Reviews></Reviews>
+        <CompanyLogs></CompanyLogs>
+        <Ackolove></Ackolove>
+        <Footer></Footer>
     </>
 }
---
+++ src/Components/Home/LandingSection/Footer.jsx
@@ -0,0 +1,108 @@
+export function Footer(){
+    return <div>
+        <div>
+        <div style={{display:"flex"}}>
+            <div style={{ width:"30%",padding:"20px 6%",boxSizing:"border-box"} }>
+               <div>
+               <p style={{fontWeight:"bolder"}}>
+                ACKO General Insurance Limited
+                </p>
+                <p style={{fontSize:"14px"}}>
+                2nd Floor, #36/5, Hustlehub One East, Somasandrapalya, 27th Main road, Sector 2, HSR Layout, Bengaluru, Karnataka - 560102
+                </p>
+               </div>
+               <div>
+                   <p style={{fontWeight:"bolder"}}>
+                   We’re socially active!
+                   </p>
+               </div>
+            </div>
+            <div style={{ width:"70%"} }>
+
+                <div style={{display:"flex"}}>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>Company</p>
+                        <p>About us</p>
+                        <p>Board of directors</p>
+                        <p>Careers</p>
+                        <p>Media kit</p>
+                        <p>Articles</p>
+                        <p>Ackology - The tech blog</p>
+                        <p>Partnerships</p>
+                        <p>Sitemap</p>
+                    </div>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>Support</p>
+                        <p>Cancellations & refunds</p>
+                        <p>Customer service</p>
+                        <p>Downloads</p>
+                        <p>Unclaimed amount</p>
+                        <p>IRDAI website</p>
+                        <p>Other Products</p>
+                    </div>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>Legal</p>
+                        <p>Whistleblower policy</p>
+                        <p>Public disclosure</p>
+                        <p>Financials & disclosures</p>
+                        <p>Privacy policy</p>
+                        <p>Terms & conditions</p>
+                        <p>Offer terms & conditions</p>
+                        <p>Stewardship code</p>
+                        <p>Disclaimer</p>
+                        <p>Anti fraud policy</p>
+                        <p>Health underwriting philosophy</p>
+                    </div>
+                   
+                </div>
+                <hr style={{width:"90%"}} />
+                <p style={{color:"rgb(138, 153, 169)",fontWeight:"bold",marginLeft:"20px"}}>Articles on insurance</p>
+                <div style={{display:"flex"}}>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>General articles</p>
+                        <p>Insurance sector in India</p>
+                        <p>Types of insurance</p>
+                        <p>Updated list of traffic rules and fines</p>
+                        <p>About your vehicle number</p>
+                        <p>What is a PUC certificate?</p>
+                        <p>Vehicle registration & re-registration</p>
+                        <p>New vehicle registration process</p>
+                        <p>Temporary vehicle registration number</p>
+                    </div>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>Car insurance articles</p>
+                        <p>Motor insurance</p>
+                        <p>Car insurance comparison</p>
+                        <p>Third party vs comprehensive</p>
+                        <p>Car insurance premium calculator</p>
+                        <p>IDV in car insurance</p>
+                        <p>Car insurance claims</p>
+                        <p>Car insurance claim settlement ratio</p>
+                        <p>Car insurance: Add-on covers</p>
+                    </div>
+                    <div style={{width:"30%", margin:"20px",fontSize:"14px"}}>
+                        <p style={{fontWeight:"bolder"}}>Two-Wheeler insurance articles</p>
+                        <p>IRDAI rules for two wheeler insurance</p>
+                        <p>Two wheeler insurance comparison</p>
+                        <p>Third party vs comprehensive</p>
+                        <p>Bike insurance premium calculator</p>
+                        <p>IDV in bike insurance</p>
+                        <p>Two wheeler insurance claims</p>
+                        <p>Two wheeler claim settlement ratio</p>
+                        <p>Two wheeler insurance: Add-on covers</p>
+                    </div>
+                   
+                </div>
+                <hr style={{width:"90%"}} />
+             
+            </div>
+        </div>
+    </div>
+    <div style={{width:"85%",margin:"auto"}}>
+        <p style={{fontWeight:"bold"}}><span>CIN: U66000KA2016PLC138288</span><samp style={{marginLeft:"40px",marginRight:"40px"}}>IRDAI Registration No: 157</samp><span>Category: Non-Life Insurance</span></p>
+        <p style={{fontSize:"12px"}}>The use of images and brands are only for the purpose of indication and illustration. ACKO claims no rights on the IP rights of any third parties. The ratings are derived from reviews and feedback received from Google and Facebook users on their respective platforms. | *Savings of upto Rs. 50,000 have been calculated on the IDV of Rs. 18,00,000 and 0% NCB. Amount saved is in comparison to tariff rates. Product name: Private Car Policy - Bundled | UIN: IRDAN157RP0014V01201819 |
+
+Trade logo displayed above belongs to ACKO Technology & Services Pvt Ltd and used by ACKO General insurance Limited under License. For more details on risk factors, terms, conditions and exclusions, please read the policy wordings carefully before concluding a sale.</p>
+    </div>
+    </div>
+}
