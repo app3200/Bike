01cfa54949794ba4ffe265183c3469b19614859c
KeshavManuja <93030991+KeshavManuja@users.noreply.github.com>
2022-02-26 16:26:16 +0530

    Backend updated
    Razorpay Integrated

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json({ extended: false }));
+
+// route included
+app.use("/payment", require("./routes/payment"))
+
+
+app.listen(port, () => console.log(`server started on port ${port}`));
